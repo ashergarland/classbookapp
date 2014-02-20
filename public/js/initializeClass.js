35fa17212a93dd2483d9434e55acdf29fd274ebf
Asher Garland <asher.garland@gmail.com>
2014-02-19 17:13:28 -0800

    added filter students functionality to class page

--- public/js/initializeClass.js
+++ public/js/initializeClass.js
@@ -1,6 +1,40 @@
+$(document).ready(function(event) {
+	students = $('table.classmates tbody tr');
+	students.filter = filterStudents;
+	//console.log(students);
 
-$('.classmates tr').click(function() {
-	var profile = $(this).attr( 'id' );
-	console.log(profile);
-	window.location.href = "profile?id=" + profile;
+	$('.classmates tr').click(function() {
+		goToProfile($(this).attr('id'));
+	});
+
+	$('#filter').keyup(function(e) {
+		students.filter($('#filter').val());
+	});
 });
+
+function goToProfile(studentId) {
+	window.location.href = "profile?id=" + profile;
+}
+
+// Hides rows that don't contain a match for the name
+// students: array of strings that contain the names of students
+// name: string to substring pattern match against list of students
+function filterStudents(name) {
+	name = name.toLowerCase();
+	this.each(function() {
+		var properties = $(this).find('td');
+		//console.log(properties);
+		var fullname = ($(properties[1]).text() + " " + $(properties[2]).text()).toLowerCase();
+		console.log(fullname.search(name));
+		console.log('fullname:' + fullname);
+		console.log('name:' + name);
+		if ( fullname.search(name) == -1 ) {
+			console.log("hide " + fullname);
+			$(this).hide();
+		}
+		else {
+			console.log("don't hide " + fullname);
+			$(this).show();
+		}
+	});
+}
